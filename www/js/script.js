abad054964378a869e445811f177151142dbb90b
Glomberg <Bazz@bk.ru>
2015-02-14 14:44:33 +0400

    READY

--- www/js/script.js
+++ www/js/script.js
@@ -37,14 +37,14 @@
 		$("#yuridicheskoe").removeClass("active");
 		$("#chastnoe").addClass("active");
 		$(".hidden").css("display","none");
-		$(".hidden2").css("display","none");
+		$(".hidden2").css("visibility","hidden");
 		$(".hidden3").css("display","block");
 	});
 	$(".yuridicheskoe").click(function(){
 		$("#chastnoe").removeClass("active");
 		$("#yuridicheskoe").addClass("active");
 		$(".hidden").css("display","block");
-		$(".hidden2").css("display","inline-block");
+		$(".hidden2").css("visibility","visible");
 		$(".hidden3").css("display","none");
 	});
 });
